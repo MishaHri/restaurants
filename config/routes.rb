bad5e45410e068bb57c4f26fda670529e20a6fce
Mihaela Hrituleac <hrituleacmihaela@yahoo.com>
2022-10-17 21:17:14 +0100

    Basic back end logic finalised

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get "restaurants", to: "restaurants#index"
 
-  get "restaurants/new", to:"restaurants#new", as: :new_resturant
+  get "restaurants/new", to:"restaurants#new"
 
   post "restaurants", to: "restaurants#create"
 
